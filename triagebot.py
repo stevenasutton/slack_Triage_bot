3b03466d3ba33d577642b26ec51798924715c676
stevenasutton <67974756+stevenasutton@users.noreply.github.com>
2020-08-12 17:03:39 -0400

    finish searching function

--- triagebot.py
+++ triagebot.py
@@ -16,18 +16,56 @@ def main():
     private_Triage = 'G016Y8CPHDH'
 
     #Get message history of the triage channel
-    hist_Triage_Channel = client.conversations_history(channel=public_Triage)
+ 
 
     def message_search():
         '''search through channel message and select
         messages without reactions and check those 
         with reactions if they have the ":heart" emoji. The heart emoji
         states that a task has been handled by a Slack administrator'''
-
+        hist_Triage_Channel = client.conversations_history(channel=public_Triage)
+        #variable to hold status of the channel have more messages to rotate through
+        has_more =True
         #Variable list to hold messages unique ids
         mess_ID = []
 
+        while has_more:
+            for messages in hist_Triage_Channel['messages']:
+                emoji_List = []
 
+                if messages.get('reactions',0) != 0:
+
+                    for emoji in messages['reactions']:
+                        emoji_List.append(emoji['name'])
+                    '''If we dont find the heart emoji the message
+                    hasn't been looked at and the team needs to be
+                    aware'''
+                    if 'heart' not in emoji_List:
+                        mess_ID.append(messages['ts'])
+
+                else:
+                    '''if there is no emoji in messages the post
+                    has not been addresed by the team'''
+                    mess_ID.append(messages['ts'])
+            '''checking to see if there is more messages to search through'''
+            if hist_Triage_Channel['has_more']:
+                '''if true update has_more variable and receive next set
+                of channel messages'''
+                has_more = hist_Triage_Channel['has_more']
+
+                hist_Triage_Channel = client.conversations_history(channel=public_Triage,cursor=\
+                                                                hist_Triage_Channel['response_metadata']\
+                                                                ['next_cursor'])
+            else:
+
+                '''There is no more messages to rotate through so close 
+                loop by updated the variable has_more'''
+                has_more= False
+
+        return mess_ID
+
+    def results():
+        return None
 
 
 if __name__ == "__main__":
